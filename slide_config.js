83b4b1be27b7a71219abe3845c0e867e5d22d99b
André Gaul <gaul@web-yard.de>
2013-02-21 15:08:13 +0100

    add template

---
+++ slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'git and github',
+    subtitle: 'work like a pro!<br>Tools seminar @ TU Berlin, February 25, 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/matheon-logo-color.svg',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    theme: ['matheon'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [
+      {
+        name: 'André Gaul',
+        company: 'TU Berlin',
+        twitter: 'andrenarchy',
+        github: 'http://github.com/andrenarchy'
+      },
+      {
+        name: 'Jan Heiland',
+        company: 'TU Berlin',
+        twitter: 'highlando',
+        github: 'http://github.com/highlando'
+      },
+      {
+        name: 'Nico Schlömer',
+        company: 'TU Berlin',
+        twitter: 'nschloe',
+        github: 'http://github.com/nschloe'
+      }
+  ]
+};
+
